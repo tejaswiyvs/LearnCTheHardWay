9412e9b892b50e26ee60e37a778571ae68d45bdf
Tejaswi Yerukalapudi <tejaswi.yvs@gmail.com>
2015-09-07 21:20:26 -0400

    push

--- ex15-a.c
+++ ex15-a.c
@@ -1,30 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-void manipulate(char *c) {
-  *c = 'b';
+void manipulate(char *s) {
+  *s = 'b';
 }
 
 int main(int argc, char *argv[]) {
-
-  char ch_a = 'a';
-  char *ch_b = &ch_a;
-  manipulate(ch_b);
-
-  printf("%c, %c\n", ch_a, *ch_b);
-
-  int a[] = { 1, 2, 3, 4, 5};
-  void *aptr = a;
-  aptr = aptr + 3;
-
-  int *bptr = aptr;
-
-  printf("%p, %p\n", a, aptr);
-
+  char *s = "str";
   int i = 0;
-  for (i = 0; i < 5; i++) {
-    printf("%d\t%d\t%d\n", bptr[i], *(a + i), *(bptr + i));
-  }
+  int *i1 = &i;
+  int **i2 = &i1;
 
+  printf("%p %p\n", i1, i2);
+  printf("%p, %p\n", i1, *i2);
   return 0;
 }
