69c47378738b669ce600ba75e1e3fec6ca68d41b
Tejaswi Yerukalapudi <tejaswi.yvs@gmail.com>
2015-09-07 22:31:07 -0400

    ex17

--- ex16.c
+++ ex16.c
@@ -17,32 +17,24 @@ typedef struct person_t {
   Gender gender;
 } Person;
 
-Person *Person_create(char *name, int age, int height, int weight, Gender g) {
-  Person *who = malloc(sizeof(Person));
-  who->name = strdup(name);
-  who->age = age;
-  who->height = height;
-  who->weight = weight;
-  who->gender = g;
+Person Person_create(char *name, int age, int height, int weight, Gender g) {
+  Person who;
+  who.name = strdup(name);
+  who.age = age;
+  who.height = height;
+  who.weight = weight;
+  who.gender = g;
   return who;
 }
 
-void Person_destroy(Person *p) {
-  assert(p != NULL);
-  free(p->name);
-  free(p);
-}
-
-void Person_print(Person *p) {
-  printf("%s - %d - %d - %d - %d\n", p->name, p->age, p->height, p->weight, p->gender);
+void Person_print(Person p) {
+  printf("%s - %d - %d - %d - %d\n", p.name, p.age, p.height, p.weight, p.gender);
 }
 
 int main(int argc, char **argv) {
-  Person *teja = Person_create("Teja", 28, 60, 170, MALE);
-  Person *boo = Person_create("Boo", 26, 60, 170, FEMALE);
+  Person teja = Person_create("Teja", 28, 60, 170, MALE);
+  Person boo = Person_create("Boo", 26, 60, 170, FEMALE);
   Person_print(teja);
   Person_print(boo);
-  Person_destroy(teja);
-  Person_destroy(boo);
   return 0;
 }
